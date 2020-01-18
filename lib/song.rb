c534a9b1496b46847bc651430660f7ad167160a0
Victoria Curl <victoriacurl9@gmail.com>
2020-01-18 17:02:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,4 +26,16 @@ end
 genre_count
 end
 
+def self.artist_count
+  artist_count = {}
+  @@artist.each do |artist|
+    if artist_count[artist]
+      artist_count[artist] += 1
+    else 
+      artist_count[artist] = 1
+    end
+  end
+  artist_count
+end
+
 end
