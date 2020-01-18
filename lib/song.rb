223ae7025b982f4a70fe9edfe4518d9917de7e01
Victoria Curl <victoriacurl9@gmail.com>
2020-01-18 16:59:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,9 +14,18 @@ def self.count
   @@count
 end
 
-def genre_count
-array = self.split (/ [\ .?!] /)
-array.length
+def self.genre_count
+genre_count = {}
+@@genres.each do |genre|
+  if genre_count[genre]
+    genre_count[genre] += 1
+  else
+    genre_count[genre] = 1
+  end
+end
+genre_count
+end
+
 end
 
 
